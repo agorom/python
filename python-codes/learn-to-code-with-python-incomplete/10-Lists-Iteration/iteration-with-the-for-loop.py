c900449ba7984cae924585b7dbc6c2545ecdc177
Onah Leo Chinagorom <agorom@agorom.leo>
2024-02-07 14:13:56 +0100

    factorial with recursion

---
+++ python-codes/learn-to-code-with-python-incomplete/10-Lists-Iteration/iteration-with-the-for-loop.py
@@ -0,0 +1,6 @@
+friends = ["Magnus", "JohnPalu", "Christopher", "Kingsley", "Divinewealth"]
+
+for friend in friends:
+	result = f"The amount of letters in {friend} is {len(friend)}"
+
+	print(result)
